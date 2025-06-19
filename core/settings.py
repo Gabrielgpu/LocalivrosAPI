b38e637e8bbdb464d2e614ab65480ab46ec75231
Gabriel <gabriel_gpu@outlook.com>
2025-06-19 18:22:38 -0300

    fix: set DEBUG to True for development environment

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-unsafe-dev-key")
 
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
