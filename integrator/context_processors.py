7beb39057002106c4a5c6c6c29af0d01ee410b4e
Gabriel <gabriel_gpu@outlook.com>
2025-06-19 00:22:38 -0300

    fix: verifica se usuario está autenticado para evitar erro.

--- integrator/context_processors.py
+++ integrator/context_processors.py
@@ -1,7 +1,9 @@
 from .models import BlingCredential
 
 def bling_credentials(request):
-    credential = BlingCredential.objects.filter(user=request.user).first()
+    credential = None
+    if request.user.is_authenticated:
+        credential = BlingCredential.objects.filter(user=request.user).first()
     return {
         'bling_credential': credential,
         'has_credential': credential is not None
